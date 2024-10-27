219f7e6c3854ed9f9f234fd4076739bcf1655052
raihanArman <raihanarman8@gmail.com>
2024-10-27 10:41:09 +0700

    deliver test load request cache retrieval

--- app/src/test/java/com/raihan/simpleplayer/GetContentLocalUseCaseTest.kt
+++ app/src/test/java/com/raihan/simpleplayer/GetContentLocalUseCaseTest.kt
@@ -1,8 +1,13 @@
 package com.raihan.simpleplayer
 
+import app.cash.turbine.test
 import io.mockk.confirmVerified
+import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Test
@@ -13,11 +18,17 @@ import org.junit.Test
  */
 
 interface ContentStore {
-    fun retrieve(): List<LocalContentModel>
+    fun retrieve(): Flow<List<LocalContentModel>>
 }
 
-class GetContentLocalUseCase {
+class GetContentLocalUseCase(
+    private val store: ContentStore
+) {
+    fun load(): Flow<List<ContentModel>> = flow {
+        store.retrieve().collect {
 
+        }
+    }
 }
 
 class GetContentLocalUseCaseTest {
@@ -26,12 +37,29 @@ class GetContentLocalUseCaseTest {
 
     @Before
     fun setup () {
-        sut = GetContentLocalUseCase()
+        sut = GetContentLocalUseCase(store)
     }
 
     @Test
     fun testInitDoesNotLoadUponCreation() = runBlocking {
         verify(exactly = 0) {
+            store.retrieve()
+        }
+
+        confirmVerified(store)
+    }
+
+    @Test
+    fun testLoadRequestCacheRetrieval() = runBlocking {
+        every {
+            store.retrieve()
+        } returns flowOf()
+
+        sut.load().test {
+            awaitComplete()
+        }
+
+        verify(exactly = 1) {
             store.retrieve()
         }
 
